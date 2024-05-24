12e8e08a556d78cad07c5f1ac5f6d35650e2488e
egendron93 <egendron93@gmail.com>
2024-05-24 18:41:44 +0700

    add a file with the calculate gsr example

---
+++ get_gsr.py
@@ -0,0 +1,60 @@
+"""
+Input a star, query the coordinates from Simbad and print out the Galactic Standard of Rest.
+
+Uses the example from:
+https://docs.astropy.org/en/latest/generated/examples/coordinates/rv-to-gsr.html#convert-a-radial-velocity-to-the-galactic-standard-of-rest-gsr
+
+"""
+
+import astropy.coordinates as coord
+import astropy.units as u
+
+coord.galactocentric_frame_defaults.set("latest")
+
+icrs = coord.SkyCoord(
+    ra=258.58356362 * u.deg,
+    dec=14.55255619 * u.deg,
+    radial_velocity=-16.1 * u.km / u.s,
+    frame="icrs",
+)
+
+def rv_to_gsr(c, v_sun=None):
+    """Transform a barycentric radial velocity to the Galactic Standard of Rest
+    (GSR).
+
+    The input radial velocity must be passed in as a
+
+    Parameters
+    ----------
+    c : `~astropy.coordinates.BaseCoordinateFrame` subclass instance
+        The radial velocity, associated with a sky coordinates, to be
+        transformed.
+    v_sun : `~astropy.units.Quantity`, optional
+        The 3D velocity of the solar system barycenter in the GSR frame.
+        Defaults to the same solar motion as in the
+        `~astropy.coordinates.Galactocentric` frame.
+
+    Returns
+    -------
+    v_gsr : `~astropy.units.Quantity`
+        The input radial velocity transformed to a GSR frame.
+
+    """
+    if v_sun is None:
+        v_sun = coord.Galactocentric().galcen_v_sun.to_cartesian()
+
+    gal = c.transform_to(coord.Galactic)
+    cart_data = gal.data.to_cartesian()
+    unit_vector = cart_data / cart_data.norm()
+
+    v_proj = v_sun.dot(unit_vector)
+
+    return c.radial_velocity + v_proj
+
+
+def main():
+    rv_gsr = rv_to_gsr(icrs)
+    print(rv_gsr)
+
+if __name__ == "__main__":
+    main()
